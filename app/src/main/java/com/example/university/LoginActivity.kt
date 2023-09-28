f21e849e9e9e3cab9e5c237b4ceae0171bce4bbd
Владислав <alinapirajoke@gmail.com>
2023-09-28 23:18:08 +0300

    complete login

--- app/src/main/java/com/example/university/LoginActivity.kt
+++ app/src/main/java/com/example/university/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.university
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
@@ -23,93 +25,122 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.university.database.DBManager
 
 class LoginActivity : AppCompatActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         val sharedPreferences =
             PreferenceManager.getDefaultSharedPreferences(this /* Activity context */)
         sharedPreferences.edit().putBoolean("session", false).apply()
         val db = DBManager(this)
-        val passwords = db.getPasswords()
         super.onCreate(savedInstanceState)
 
         setContent() {
             val context = LocalContext.current
-            Row(
+            val keyboardController = LocalSoftwareKeyboardController.current
+
+            Column(
                 Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp),
-                horizontalArrangement = Arrangement.Center
+                    .fillMaxHeight()
+                    .padding(50.dp, 0.dp),
+                verticalArrangement = Arrangement.Center,
             ) {
+                Box() {
 
-                Column(
-                    Modifier
-                        .fillMaxHeight()
-                        .padding(15.dp),
-                    verticalArrangement = Arrangement.Center,
-                ) {
-                    Box() {
+                    Column(
+                        Modifier
+                            .fillMaxHeight()
+                            .padding(15.dp),
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            text = "Вы не авторизованы",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 20.dp),
+                            textAlign = TextAlign.Center,
+                            //style = TextStyle(fontWeight = FontWeight.Bold),
+                            fontSize = 20.sp,
+                        )
 
-                        Column(
-                            Modifier
-                                .fillMaxHeight()
-                                .padding(15.dp),
-                            verticalArrangement = Arrangement.Center,
+                        var pass by remember { mutableStateOf("") }
+                        OutlinedTextField(
+                            value = pass,
+                            onValueChange = { pass = it },
+                            label = {
+                                Text("Введите пароль")
+                            },
+                            modifier = Modifier.padding(bottom = 20.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Ascii,
+                                imeAction = ImeAction.Done
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onDone = {
+                                    keyboardController?.hide()
+                                    checkPassword(pass, db, context, sharedPreferences)
+                                })
+                        )
+
+                        Button(
+                            onClick = {
+                                checkPassword(pass, db, context, sharedPreferences)
+                            },
+                            modifier = Modifier.fillMaxWidth()
                         ) {
-                            Text(
-                                text = "Вы не авторизованы",
-                                Modifier
-                                    .padding(bottom = 20.dp),
-                                textAlign = TextAlign.Center
-                            )
 
-                            var pass by remember { mutableStateOf("") }
-                            OutlinedTextField(value = pass,
-                                onValueChange = { pass = it },
-                                label = { Text("Введите пароль")
-                                },
-                                modifier = Modifier.padding(bottom = 20.dp),
-                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-                            )
+                            Text(text = "Войти")
+                        }
 
-                            Button(
-                                onClick = {
-                                    if (pass in db.getPasswords().keys) {
-                                        passwords.get(pass)?.let {
-                                            sharedPreferences.edit().putInt("user", it).apply()
-                                        }
-                                        sharedPreferences.edit().putBoolean("session", true).apply()
-                                        toMain()
-                                    } else {
-                                        showToast("Пароль не верен", context)
-                                    }
-                                },
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-
-                                Text(text = "Войти")
-                            }
-
-                            Button(onClick = {
+                        Button(
+                            onClick = {
                                 toRegistration()
                             },
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
 
-                                Text(text = "Создать нового пользователя")
-                            }
+                            Text(text = "Создать нового пользователя")
                         }
                     }
                 }
+
             }
         }
 
+    }
+
+    private fun checkPassword(
+        pass: String,
+        db: DBManager,
+        context: Context,
+        sharedPreferences: SharedPreferences
+    ) {
+        if (pass.isEmpty()) {
+            showToast("Введите пароль", context)
+            return
+        }
+        val passwords = db.getPasswords()
+        if (pass in passwords.keys) {
+            passwords.get(pass)?.let {
+                sharedPreferences.edit().putInt("user", it).apply()
+            }
+            sharedPreferences.edit().putBoolean("session", true).apply()
+            toMain()
+        } else {
+            showToast("Пароль не верен", context)
+        }
     }
 
     fun showToast(text: String, context: Context) {
--- app/src/main/java/com/example/university/RegistrationActivity.kt
+++ app/src/main/java/com/example/university/RegistrationActivity.kt
@@ -2,41 +2,55 @@ package com.example.university
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.university.database.DBManager
+import kotlinx.coroutines.launch
 
 class RegistrationActivity : AppCompatActivity() {
+    val TAG = "RegistrationActivity"
+
     @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,90 +60,126 @@ class RegistrationActivity : AppCompatActivity() {
             PreferenceManager.getDefaultSharedPreferences(this /* Activity context */)
 
         setContent() {
-            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+            var parentSize by remember {
+                mutableStateOf(Size.Zero)
+            }
             val context = LocalContext.current
             val keyboardController = LocalSoftwareKeyboardController.current
             val focusManager = LocalFocusManager.current
-            Row(
+
+
+            val scrollState = rememberScrollState()
+            Column(
                 Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp),
-                horizontalArrangement = Arrangement.Center
+                    .fillMaxHeight()
+                    .padding(50.dp, 0.dp)
+                    .verticalScroll(scrollState),
+                verticalArrangement = Arrangement.Center
+
+
             ) {
+                Text(
+                    text = "Создайте нового пользователя",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    textAlign = TextAlign.Center,
+                    //style = TextStyle(fontWeight = FontWeight.Bold),
+                    fontSize = 20.sp,
+                )
 
-                Column(
-                    Modifier
-                        .fillMaxHeight()
-                        .padding(15.dp),
-                    verticalArrangement = Arrangement.Center
+                var pass1 by remember { mutableStateOf("") }
+                OutlinedTextField(
+                    value = pass1,
+                    onValueChange = { pass1 = it },
+                    label = { Text("Придумайте пароль") },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Ascii,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    }),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
 
+                var pass2 by remember { mutableStateOf("") }
+                OutlinedTextField(
+                    value = pass2,
+                    onValueChange = { pass2 = it },
+                    label = { Text("Повторите пароль") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Ascii,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            keyboardController?.hide()
+                            addUser(pass1, pass2, db, context, sharedPreferences)
+                        })
+                )
+
+                Button(
+                    onClick = {
+                        addUser(pass1, pass2, db, context, sharedPreferences)
+                    },
+                    modifier = Modifier.fillMaxWidth()
                 ) {
-                    Text(
-                        text = "Создайте нового пользователя",
-                        modifier = Modifier.padding(bottom = 20.dp),
-                    )
 
-                    var pass1 by remember { mutableStateOf("") }
-                    OutlinedTextField(value = pass1,
-                        onValueChange = { pass1 = it },
-                        label = { Text("Придумайте пароль") },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Ascii,
-                            imeAction = ImeAction.Done),
-                        keyboardActions = KeyboardActions(
-                            onDone = {keyboardController?.hide()})
-                    )
+                    Text(text = "Создать нового пользователя")
+                }
 
-                    var pass2 by remember { mutableStateOf("") }
-                    OutlinedTextField(
-                        value = pass2,
-                        onValueChange = { pass2 = it },
-                        label = { Text("Повторите пароль") },
-                        modifier = Modifier.padding(bottom = 20.dp),
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Ascii,
-                            imeAction = ImeAction.Next),
-                        keyboardActions = KeyboardActions(onNext = {
-                            focusManager.moveFocus(FocusDirection.Down)
-                        }),
+                Button(
+                    onClick = {
+                        toLogin()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
 
-                    )
-
-                    Button(
-                        onClick = {
-                            if (pass1 == pass2) {
-                                if ((pass1 in db.getPasswords().keys) == false && isValidPassword(pass1) ) {
-                                    db.insertPassword(pass1)
-                                    val newId = db.getPasswords().get(pass1)
-                                    sharedPreferences.edit().putBoolean("session", true).apply()
-                                    newId?.let {
-                                        sharedPreferences.edit().putInt("user", it).apply()
-                                    }
-                                    toMain()
-                                } else {
-                                    showToast("Пароль не подходит, придумайте другой", context)
-                                }
-                            } else {
-                                showToast("Пароли не совпадают", context)
-                            }
-                        },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-
-                        Text(text = "Создать нового пользователя")
-                    }
-
-                    Button(
-                        onClick = {
-                            toLogin()
-                        },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-
-                        Text(text = "Войти")
-                    }
+                    Text(text = "Войти")
                 }
             }
+
         }
+    }
+
+    fun addUser(
+        pass1: String,
+        pass2: String,
+        db: DBManager,
+        context: Context,
+        sharedPreferences: SharedPreferences
+    ) {
+        if (pass1 == pass2) {
+            if ((pass1 in db.getPasswords().keys) == false && isValidSymbols(pass1)) {
+                db.insertPassword(pass1)
+                val newId = db.getPasswords().get(pass1)
+                sharedPreferences.edit().putBoolean("session", true).apply()
+                newId?.let {
+                    sharedPreferences.edit().putInt("user", it).apply()
+                }
+                toMain()
+            } else {
+                showToast("Пароль не подходит, придумайте другой", context)
+                Log.w(TAG, "Пароль не подошел, контекст: pass: $pass1, " +
+                            "совпадение паролей: ${(pass1 in db.getPasswords().keys)}, " +
+                            "Валидность пароля: ${isValidSymbols(pass1)}")
+            }
+        } else {
+            showToast("Пароли не совпадают", context)
+        }
+    }
+
+    private fun isValidSymbols(password: String): Boolean {
+        val passwordRegex = Regex("^[a-zA-Z0-9]*$")
+        Log.d(TAG, "Проверка пароля: " +
+                "валидность символов: ${password.matches(passwordRegex)}, " +
+                "длинна: ${password.length}")
+        return (password.matches(passwordRegex) && password.length >= 6)
     }
 
     fun showToast(text: String, context: Context) {
@@ -138,11 +188,6 @@ class RegistrationActivity : AppCompatActivity() {
             text,
             Toast.LENGTH_SHORT
         ).show()
-    }
-
-    private fun isValidPassword(password: String): Boolean {
-        val passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}\$"
-        return (password.matches(passwordRegex.toRegex()) && password.length > 6)
     }
 
     fun toLogin() {
